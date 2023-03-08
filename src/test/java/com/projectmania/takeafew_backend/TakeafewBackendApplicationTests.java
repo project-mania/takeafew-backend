0cfddf88fcb10757358e0de4722d303163aa8d85
kudzait <kudzai.tsapo@boxfusion.io>
2023-03-08 19:37:35 +0200

    Initialisation of files

---
+++ src/test/java/com/projectmania/takeafew_backend/TakeafewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectmania.takeafew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeafewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
